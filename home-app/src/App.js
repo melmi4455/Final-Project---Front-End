7807bae89d05dbe027e802f4d642e5a980d52473
GuleidAbdi <guleidac@gmail.com>
2022-08-16 00:46:55 +0300

    designed cards

--- home-app/src/App.js
+++ home-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Pages/Login";
+
 import Home from "./Pages/Home";
 import Header from "./Components/Header";
 import SignUp from "./Pages/SignUp";
+import Details from "./Pages/Details";
 import PropertyList from "./Pages/PropertyList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -17,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
 
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/Details" element={<Details />} />
 
           <Route path="/list" element={<PropertyList />} />
         </Routes>
---
+++ home-app/src/Components/HowItWorks.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <div className=" flex justify-center px-3 py-6">
+      <div className="grid grid-cols-2 gap-10">
+        <div className="flex  items-center space-x-2">
+          <img
+            src={require("../img/search.png")}
+            alt="home"
+            className="w-20 h-12"
+          />
+          <div>
+            <h3>
+              With our immense real-time rental database<br></br>
+              and advanced search, we make it easy to find<br></br>
+              the perfect Boston apartment.<br></br>
+            </h3>
+            <button className="bg-blue-400 text-white font-bold text-sm rounded-md p-2 m-2">
+              Search for Rents
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img
+            src={require("../img/home.jpg")}
+            alt="home"
+            className="w-20 h-12"
+          />
+          <div>
+            <h3>
+              Discover your new home or condo using our targeted<br></br>
+              filters Browse images, <br></br>floor plans, video, and 3D tours
+            </h3>
+
+            <button className="bg-blue-400 text-white font-bold text-sm rounded-md p-2 m-2">
+              Search for Rents
+            </button>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <img
+            src={require("../img/hand.jpg")}
+            alt="home"
+            className="w-20 h-12"
+          />
+          <div>
+            <h3>
+              home rental is a premier real estate portal for<br></br>
+              property owners to maximize exposure and<br></br>
+              lease properties faster.
+            </h3>
+            <button className="bg-blue-400 text-white font-bold text-sm rounded-md p-2 m-2">
+              Search for Rents
+            </button>
+          </div>
+        </div>
+        <div className="flex  items-center space-x-2">
+          <img
+            src={require("../img/hand.jpg")}
+            alt="home"
+            className="w-20 h-12"
+          />
+          <div>
+            <h3>
+              home rental is a premier real estate portal for<br></br>
+              property owners to maximize exposure and<br></br>
+              lease properties faster.
+            </h3>
+            <button className="bg-blue-400 text-white font-bold text-sm rounded-md p-2 m-2">
+              Search for Rents
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWorks;
